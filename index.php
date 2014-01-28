0c585d908834f360c103e04898243d1469ae2a77
Jaume Cornadó <jaume@bazingasystems.com>
2014-01-28 19:37:11 +0100

    Modified Structure and GET, POST, DELETE functional

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once('config.php');
-require_once('ArrestDB.php');
-require_once('ApiResponse.php');
-require_once('SwaggerHelper.php');
-require_once('ArrestDBController.php');
+require_once(__DIR__.'/config.php');
+require_once(__DIR__.'/include/ArrestDB.php');
+require_once(__DIR__.'/include/ApiResponse.php');
+require_once(__DIR__.'/include/SwaggerHelper.php');
+require_once(__DIR__.'/include/ResterController.php');
 
 /**
 * The MIT License
@@ -19,14 +19,7 @@ if (strcmp(PHP_SAPI, 'cli') === 0)
 	exit('ArrestDB should not be run from CLI.' . PHP_EOL);
 }
 
-$restController = new ArrestDBController();
-
-//Check the client restrictions
-$restController->checkClientRestriction();
-
-//Test the sql server
-$restController->checkConnectionStatus();
-
+$resterController = new ResterController();
 
 if (array_key_exists('_method', $_GET) === true)
 {
@@ -38,6 +31,29 @@ else if (array_key_exists('HTTP_X_HTTP_METHOD_OVERRIDE', $_SERVER) === true)
 	$_SERVER['REQUEST_METHOD'] = strtoupper(trim($_SERVER['HTTP_X_HTTP_METHOD_OVERRIDE']));
 }
 
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+
+/*
+$resterController->addRequestProcessor("GET", "/(#any)/list", function($route) {
+	global $resterController;
+	$resterController->getAllObjectsFromRoute($route);
+});
+*/
+
+/*
+$resterController->addRequestProcessor("GET", "/", function() {
+	$rc = new ResterController();
+	$routes = $rc->getAvailableRoutes();
+	$result = SwaggerHelper::routeResume($routes);
+	return ArrestDB::Reply($result);
+});
+*/
+
+
+//Do the work
+$resterController->processRequest($requestMethod);
+
+/*
 ArrestDB::Serve('GET', '/(#any)/(#any)/(#any)', function ($table, $id, $data)
 {
 
@@ -98,65 +114,7 @@ ArrestDB::Serve('GET', '/(#any)/(#any)/(#any)', function ($table, $id, $data)
 	return ArrestDB::Reply($result);
 });
 
-ArrestDB::Serve('GET', '/(#any)/(#any)?', function ($table, $id = null)
-{
-		if (isset($id) === true)
-		{
-			$query = array
-			(
-				sprintf('SELECT * FROM "%s"', $table),
-				);
-	
-				if($id != "list")
-					$query[] = sprintf('WHERE "%s" = ? LIMIT 1', 'id');
-	
-	/*else
-	{*/
-		if (isset($_GET['by']) === true)
-		{
-			if (isset($_GET['order']) !== true)
-			{
-				$_GET['order'] = 'ASC';
-			}
 
-			$query[] = sprintf('ORDER BY "%s" %s', $_GET['by'], $_GET['order']);
-		}
-
-		if (isset($_GET['limit']) === true)
-		{
-			$query[] = sprintf('LIMIT %u', $_GET['limit']);
-
-			if (isset($_GET['offset']) === true)
-			{
-				$query[] = sprintf('OFFSET %u', $_GET['offset']);
-			}
-		}
-	//}
-
-	$query = sprintf('%s;', implode(' ', $query));
-
-	$result = (isset($id) === true && $id != "list") ? ArrestDB::Query($query, $id) : ArrestDB::Query($query);
-
-	if ($result === false)
-	{
-		$result = ApiResponse::errorResponse(404);
-	}
-
-	else if (empty($result) === true)
-	{
-		$result = ApiResponse::errorResponse(204);
-	}
-
-	else if (isset($id) === true)
-	{
-		$result = array_shift($result);
-	}
-
-	return ArrestDB::Reply($result);
-	} else {
-		return ArrestDB::Reply(SwaggerHelper::getDocFromRoute($table, ArrestDBController::getAvailableRoutes()));
-	}
-});
 
 ArrestDB::Serve('DELETE', '/(#any)/(#num)', function ($table, $id)
 {
@@ -235,100 +193,7 @@ if (in_array($http = strtoupper($_SERVER['REQUEST_METHOD']), array('POST', 'PUT'
 	unset($data);
 }
 
-ArrestDB::Serve('POST', '/(#any)', function ($table)
-{
-	if (empty($_POST) === true)
-	{
-		$result = array
-		(
-			'error' => array
-			(
-				'code' => 204,
-				'status' => 'No Content',
-			),
-		);
-	}
 
-	else if (is_array($_POST) === true)
-	{
-		$queries = array();
-
-		if (count($_POST) == count($_POST, COUNT_RECURSIVE))
-		{
-			$_POST = array($_POST);
-		}
-
-		foreach ($_POST as $row)
-		{
-			$data = array();
-
-			foreach ($row as $key => $value)
-			{
-				$data[sprintf('"%s"', $key)] = '?';
-			}
-
-			$query = array
-			(
-				sprintf('INSERT INTO "%s" (%s) VALUES (%s)', $table, implode(', ', array_keys($data)), implode(', ', $data)),
-			);
-
-			$queries[] = array
-			(
-				sprintf('%s;', implode(' ', $query)),
-				$data,
-			);
-		}
-
-		if (count($queries) > 1)
-		{
-			ArrestDB::Query()->beginTransaction();
-
-			while (is_null($query = array_shift($queries)) !== true)
-			{
-				if (($result = ArrestDB::Query($query[0], $query[1])) === false)
-				{
-					ArrestDB::Query()->rollBack(); break;
-				}
-			}
-
-			if (($result !== false) && (ArrestDB::Query()->inTransaction() === true))
-			{
-				$result = ArrestDB::Query()->commit();
-			}
-		}
-
-		else if (is_null($query = array_shift($queries)) !== true)
-		{
-			$result = ArrestDB::Query($query[0], $query[1]);
-		}
-
-		if ($result === false)
-		{
-			$result = array
-			(
-				'error' => array
-				(
-					'code' => 409,
-					'status' => 'Conflict',
-				),
-			);
-		}
-
-		else
-		{
-			$result = array
-			(
-				'success' => array
-				(
-					'code' => 201,
-					'status' => 'Created',
-				),
-			);
-		}
-	}
-
-	return ArrestDB::Reply($result);
-});
 
 ArrestDB::Serve('PUT', '/(#any)/(#num)', function ($table, $id)
 {
@@ -391,34 +256,11 @@ ArrestDB::Serve('PUT', '/(#any)/(#num)', function ($table, $id)
 
 //Get the api root
 ArrestDB::Serve('GET', NULL, function () {
-	
-	/*$result = ArrestDB::Query("SHOW TABLES");
-	
-	if ($result === false)
-	{
-		$result = ApiResponse::errorResponse(404);
-	}
-
-	else if (empty($result) === true)
-	{
-		$result = ApiResponse::errorResponse(204);
-	}
-
-	else 
-	{
-		$routes = array();
-	
-		foreach($result as $k => $v) {
-			$routes[] = reset($v);	
-		}
-		
-		$result = SwaggerHelper::routeResume($routes);
-	}*/
-
-	$routes = ArrestDBController::getAvailableRoutes();
+	$resterController = new ResterController();
+	$routes = $resterController->getAvailableRoutes();
 	$result = SwaggerHelper::routeResume($routes);
 	return ArrestDB::Reply($result);
-});
+});*/
 
 $result = ApiResponse::errorResponse(400);
 
